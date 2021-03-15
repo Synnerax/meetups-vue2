c9e82efdc9fb5902ec9a4ebe5eeaba85c2af96a4
jensengbg-elliot-berggren <elliot.berggren@hotmail.com>
2021-03-15 16:44:00 +0100

    Added test to check for review section when mounted

--- tests/unit/Review.spec.js
+++ tests/unit/Review.spec.js
@@ -2,4 +2,34 @@ import { mount, shallowMount } from "@vue/test-utils";
 import Review from "@/components/Review.vue";
 
 
+describe("Review.vue", () => {
+    it("should check if review section is displayed", async () => {
+        const wrapper = shallowMount(Review)
+
+        const reviewSection = wrapper.find(".review")
+        
+        expect(reviewSection.exists()).toBeTruthy()
+
+
+    
+    })
+
+    it("should display all saved comments ", async () => {
+
+        const wrapper = shallowMount(Review, {
+            propsData:{
+                reviews: [
+                    {
+                        name: "fake name",
+                        comment: "some text left by the reviewer"
+                    }
+                ]
+            }
+        })
+
+        const reviewName = wrapper.find(".rewview-name")
+        const reviewComment = wrapper.find(".review-comment")
+    })
+})
+
 // should let you write a comment
